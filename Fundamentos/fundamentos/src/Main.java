a9f78f3365d663627d52ba2d7acc9ca35085f830
KayllaneGPina <kayllanegfpina@gmail.com>
2023-10-05 13:48:18 -0300

    first commit

---
+++ Fundamentos/fundamentos/src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+// Classe
+public class Main {
+
+    //Métodos
+    public static void main(String[] args) {
+        System.out.print("Hello, world!");
+    }
+}
